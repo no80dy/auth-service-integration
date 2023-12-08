7eb1d21fb973a5b6886d8f6360e49bbd7a6f5368
no80dy <no8odyas@gmail.com>
2023-12-08 21:04:10 +0300

    Добавил ограничение запросов к сервису выдачи фильмов

--- movie_service/src/main.py
+++ movie_service/src/main.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
+from datetime import datetime
 
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi import Request, status
 from fastapi.responses import JSONResponse
 
 from api.v1 import films, genres, persons
@@ -13,6 +15,9 @@ from db.elastic import ElasticStorage
 
 from db import cache
 from db import storage
+
+
+REQUEST_LIMIT_PER_MINUTE = 20
 
 
 @asynccontextmanager
@@ -40,6 +45,24 @@ app = FastAPI(
     default_response_class=JSONResponse,
     lifespan=lifespan
 )
+
+
+@app.middleware('http')
+async def rate_limit(request: Request, call_next):
+    response = await call_next(request)
+    request_id = request.headers.get('X-Request-Id')
+    pipe = await cache.cache.pipeline()
+    now = datetime.now()
+
+    key = f'{request_id}:{now.minute}'
+    pipe.incr(key, 1)
+    pipe.expire(key, 59)
+    result = await pipe.execute()
+    request_number = result[0]
+
+    if request_number > REQUEST_LIMIT_PER_MINUTE:
+        return JSONResponse(status_code=status.HTTP_429_TOO_MANY_REQUESTS, content='Rate limit exceeded')
+    return response
 
 
 app.include_router(films.router, prefix='/movie_service/api/v1/films', tags=['films'])
